bd8c763cb5be59564e777a6e24b62e5740f62033
clod44 <Rt_Sayochi@hotmail.com>
2023-12-09 17:18:53 +0300

    chapter 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+// rootlayout will have children with the type of "React.ReactNode" and you just render them 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
